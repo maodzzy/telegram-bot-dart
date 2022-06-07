c18ad0a16933522e78122f8fe9dea1cb6c20c883
Ruslan Torshin <r.torshin@abcmobile.com>
2022-06-07 18:11:28 +0300

    initial commit

---
+++ bin/telegram_dart_bot.dart
@@ -0,0 +1,5 @@
+import 'package:telegram_dart_bot/telegram_dart_bot.dart' as telegram_dart_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${telegram_dart_bot.calculate()}!');
+}
---
+++ test/telegram_dart_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:telegram_dart_bot/telegram_dart_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
